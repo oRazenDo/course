2db5cf2e665db54f1188bbc02b725c13bf0afd71
oRazenDo <antonbolgov.98@gmail.com>
2020-04-22 07:26:56 -0500

    Updated

--- Les5/ex1.py
+++ Les5/ex1.py
@@ -1,23 +1,39 @@
-class Whois():
+class People():
 
-    def __init__(self, name, age, speciality, grade):
+    def __init__(self, name, age, speciality):
         self.name = name
         self.age = age
         self.speciality = speciality
-        self.grade = grade
 
+class Student(People):
+    def __init__(self,name, age, speciality, gradeStud ):
+        super().__init__(name, age, speciality)
+        self.gradeStud = gradeStud
     def where(self):
-        if self.speciality == 'student':
-            return "%s in class" % self.name
-        elif self.speciality == 'teacher':
-            return "%s at work" % self.name
-
+        return "%s in class" % self.name
     def rank(self):
-        return "rank %s as a %s is %s" % (self.name, self.speciality, self.grade)
+        return "Rank %s as a %s is %s" % (self.name, self.speciality, self.gradeStud)
+    def pay(self):
+        self.gradeStud = self.gradeStud*10
+        return "Pay is %s" % self.gradeStud
 
-fonk = Whois('Fonk',22,'student',4)
+class Teacher(People):
+    def __init__(self,name, age, speciality, gradeTeach ):
+        super().__init__(name, age, speciality)
+        self.gradeTeach = gradeTeach
+    def where(self):
+        return "%s at work" % self.name
+    def rank(self):
+        return "Rank %s as a %s is %s" % (self.name, self.speciality, self.gradeTeach)
+    def pay(self):
+        self.gradeTeach = self.gradeTeach*100
+        return "Pay is %s " % self.gradeTeach
+
+fonk = Student('Fonk',22,'student',4)
 print(fonk.where())
-print(fonk.rank())
-grace = Whois('Grace',35,'teacher',8)
+print(fonk.rank(),'and',fonk.pay())
+grace = Teacher('Grace',35,'teacher',8)
 print(grace.where())
-print(grace.rank())
+print(grace.rank(),'and',grace.pay())
+wizzy = People('Wizzy',29,'student')
+print(wizzy.where())
